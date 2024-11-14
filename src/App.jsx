a7a682bd90c56d4ba82aa27c44ca05eb0685e785
Yash Thakare <109447830+yashthakare0@users.noreply.github.com>
2024-11-14 19:05:22 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import React, { useState,useEffect } from "react";
+import Navbar from "../src/components/navbar";
+import MainWeatherCard from "../src/components/mainweathercard";
+import FiveDayForecast from "../src/components/fiveday";
+import TodayHighlights from "../src/components/todayhighlights";
+
+import axios from "axios";
+
+const WeatherDashboard = () => {
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('London'); // Default city is set to London
+  const [airQualityData, setAirQualityData] = useState(null);
+  const [fiveDayForecast, setFiveDayForecast] = useState(null);
+
+  useEffect(() => {
+    fetchWeatherData(city);
+  }, [city]);
+
+  const fetchAirQualityData = (lat, lon) => {
+    const API_KEY = '0198cd08d3848ac86de381ed8c9153ba'; // Replace with your OpenWeatherMap API key
+    axios.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+      .then(response => {
+        setAirQualityData(response.data.list[0]); // Set the first item in the list as air quality data
+      })
+      .catch(error => console.error('Error fetching the air quality data:', error));
+  };
+
+  const fetchWeatherData = (city) => {
+    const API_KEY = '0198cd08d3848ac86de381ed8c9153ba'; // Replace with your OpenWeatherMap API key
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        setWeatherData(data);
+        console.log(JSON.stringify(data));
+        fetchAirQualityData(data.coord.lat, data.coord.lon); 
+        axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`)
+        .then(response => {
+          setFiveDayForecast(response.data);
+        })
+        .catch(error => console.error('Error fetching the 5-day forecast data:', error));
+    
+      })
+      .catch(error => console.error('Error fetching the weather data:', error));
+  };
+
+  const handleSearch = (searchedCity) => {
+    setCity(searchedCity); 
+  };
+
+
+  return (
+    <div>
+      <Navbar onSearch={handleSearch} />
+      {weatherData && airQualityData &&  (
+        <div style={{ display: "flex", padding: "30px", gap: "20px" }}>
+          <div style={{ flex: "1", marginRight: "10px" }}>
+            <MainWeatherCard weatherData={weatherData} />
+            <p style={{ fontWeight: "700", fontSize: "20px", marginTop: "20px" }}>5 Days Forecast</p>
+            {fiveDayForecast && <FiveDayForecast forecastData={fiveDayForecast} />}
+          </div>
+          <div style={{ display: "flex", flexDirection: "column", flex: "0.5", gap: "20px" }}>
+            <TodayHighlights weatherData={weatherData} airQualityData={airQualityData}  />
+            
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WeatherDashboard;
